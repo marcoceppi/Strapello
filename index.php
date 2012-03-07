16c698dbe3ec7353373f413df8e7b34fe419b26e
Marco Ceppi <marco@ceppi.net>
2012-03-07 14:55:54 -0500

    Added Route parsing

--- index.php
+++ index.php
@@ -7,3 +7,19 @@
  * do all this jazz. But! WE'RE PROTOTYPING ON THE FLY GOGOGO. Proof of Concept <3
  */
  
+$raw_route = (!empty($_GET['__r'])) ? $_GET['__r'] : 'main';
+
+$routes = explode('/', $raw_route);
+$route = array_shift($routes);
+
+if( file_exists('app/' . $route . '.php') )
+{
+	require_once('app/' . $route . '.php');
+	
+	$method = (!empty($routes)) ? array_shift($routes) : 'init';
+	$route::$method($routes);
+}
+else
+{
+	die('Lost?');
+}
