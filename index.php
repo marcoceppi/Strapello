05b70fd55712fffc5dd187cf7e28e89a02c3c79d
Marco Ceppi <marco@ceppi.net>
2012-03-07 14:34:36 -0500

    Added Router start

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Main router
+ *
+ * So many people have frameworks out there, including one I built to
+ * do all this jazz. But! WE'RE PROTOTYPING ON THE FLY GOGOGO. Proof of Concept <3
+ */
+ 
