4a304fb2a8077b5ff774900bf266ba6405e7e73e
Marco Ceppi <marco@ceppi.net>
2012-03-07 17:35:44 -0500

    Added methods for Trello

--- lib/helper/Trello.php
+++ lib/helper/Trello.php
@@ -7,3 +7,21 @@
  * @author Marco Ceppi <marco@ceppi.net>
  */
 
+class Trello
+{
+	// Hello o/
+	
+	// We *really* need to be caching data more aggressively
+	private $cache = array();
+	
+	public static function fetch($url)
+	{
+		return @file_get_contents($url);
+	}
+	
+	public static function get($path)
+	{
+		
+	}
+}
+
