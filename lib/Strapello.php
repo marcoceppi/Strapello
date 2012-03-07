6d1caa92ba46ab5e3722b5b7979f4f80cc04ba95
Marco Ceppi <marco@ceppi.net>
2012-03-07 14:20:11 -0500

    Added skeleton Classes

---
+++ lib/Strapello.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Strapello
+ * 
+ * @package Strapello
+ * @subpackge Model
+ * @author Marco Ceppi <marco@ceppi.net>
+ */
+
+require_once('Trello.php');
+
+class Strapello
+{
+	public static $lists, $boards, $members, $cards;
+	protected static $trello;
+	private static $cache;
+	
+	public static function list($id)
+	{
+		
+	}
+	
+	public static function board($id)
+	{
+		
+	}
+	
+	public static function member($id)
+	{
+		
+	}
+	
+	/**
+	 * Cards
+	 * 
+	 * Get the cards for a member, organization, board, or list
+	 * 
+	 * @param string $key either a member, organization, board, list
+	 * @param mixed optional $value The search value for key
+	 * 
+	 * @return false|array of $key cards
+	 */
+	public static function cards($key, $value = null)
+	{
+		
+	}
+	
+	public static function card()
+	{
+		
+	}
+}
---
+++ lib/Trello.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Trello API
+ * 
+ * @package Trello
+ * @subpackge API
+ * @author Marco Ceppi <marco@ceppi.net>
+ */
+
